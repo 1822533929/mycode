aceb91fc9ec5e5b80d8d79403724d410ada5e35f
Qiu JiaNan <1822533929@qq.com>
2024-07-12 11:41:15 +0800

    测试

--- android_connection/src/main/java/com/example/android_connection/service/serviceImpI/OrderServiceImpl.java
+++ android_connection/src/main/java/com/example/android_connection/service/serviceImpI/OrderServiceImpl.java
@@ -1,16 +1,18 @@
 package com.example.android_connection.service.serviceImpI;
 
+import com.example.android_connection.Bean.orderDetail;
 import com.example.android_connection.service.OrdersService;
 import com.mysql.cj.x.protobuf.MysqlxCrud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderServiceImpl {
+public class OrderServiceImpl implements  OrdersService{
     @Autowired
     OrdersService ordersService;
-    public void getOrderDetail(int id)
+    public orderDetail getOrderDetail(int id)
     {
-        ordersService.getOrderDetail(id);
+        System.out.println("service");
+        return ordersService.getOrderDetail(id);
     }
 }
